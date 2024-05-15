0ed9d11a7cdc08d89cbb431fa66118d343a73c48
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2024-05-15 18:11:38 -0300

    Adjusting and creating a second var for response

--- users/put.go
+++ users/put.go
@@ -7,12 +7,13 @@ import (
 
 func updateUserById(c fiber.Ctx) error {
 	body := new(User)
+	result := User{}
 	if err := c.Bind().Body(body); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
-	err := db.UpdateByID("users", c.Params("id"), body, body)
+	err := db.UpdateByID("users", c.Params("id"), body, &result)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
-	return c.JSON(body)
+	return c.JSON(result)
 }
