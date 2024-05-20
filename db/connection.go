4f52ed722990dba7f7eae45bbe60c12b55c8a9f8
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2024-05-20 13:54:12 -0300

    Using envs for mongodb uri

--- db/connection.go
+++ db/connection.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,12 +11,17 @@ import (
 )
 
 const (
-	DBNAME = "todos"
+	DBNAME               = "todos"
+	DEFAULT_DATABASE_URI = "mongodb://root:example@localhost:27017/?authSource=admin"
 )
 
 func GetConnection() (client *mongo.Client, ctx context.Context) {
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = DEFAULT_DATABASE_URI
+	}
 	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017/?authSource=admin"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
