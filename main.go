2730f054277bbc91d2da224683be766aee1835a7
Nenodias <horacio.dias92@gmail.com>
2024-05-28 23:56:18 -0300

    Adding new routes to main

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/nenodias/go-mongodb-todo/tags"
+	"github.com/nenodias/go-mongodb-todo/tasks"
 	"github.com/nenodias/go-mongodb-todo/users"
 )
 
@@ -11,5 +13,7 @@ func main() {
 	app := fiber.New()
 	v1 := app.Group("/v1")
 	users.SetRoutes(v1)
+	tasks.SetRoutes(v1)
+	tags.SetRoutes(v1)
 	log.Fatal(app.Listen(":8000"))
 }
